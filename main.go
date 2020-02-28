ecb78d955cb4ae35d57efb9acd9e68630f0414ca
novalagung <caknopal@gmail.com>
2020-02-28 14:38:53 +0700

    add flag instance-id

--- main.go
+++ main.go
@@ -8,11 +8,12 @@ import (
 
 func main() {
 	flagPort := flag.String("port", "8080", "web server port")
+	flagInstanceID := flag.String("instance-id", "", "instance id")
 	flag.Parse()
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world"))
+		w.Write([]byte("hello world. from " + *flagInstanceID))
 	})
 	server := new(http.Server)
 	server.Handler = mux
