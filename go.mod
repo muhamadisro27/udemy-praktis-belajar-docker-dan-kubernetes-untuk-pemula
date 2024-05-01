5b4596abd79fe2e4b33c96994e3a0645f0f9170b
muhamadisro27 <mohammadisro2710@gmail.com>
2024-05-01 18:51:46 +0700

    feat: set up orchestration with docker compose

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module hello-world
 
 go 1.13
+
+require github.com/go-sql-driver/mysql v1.8.1 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
+
+	"encoding/json"
 )
 
 func main() {
@@ -28,6 +32,19 @@ func main() {
 
 		w.Write([]byte(text))
 	})
+
+	mux.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+
+		case "GET":
+			getAllUsersHandler(w,r)
+		case "POST":
+			createUserHandler(w,r)
+		default:
+			http.Error(w, "http method not allowed", http.StatusBadRequest)
+		}
+	})
+
 	server := new(http.Server)
 	server.Handler = mux
 	server.Addr = "0.0.0.0:" + port
@@ -37,4 +54,85 @@ func main() {
 	if err != nil {
 		log.Fatal(err.Error())
 	}
+}
+
+func getAllUsersHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := conn()
+
+	if err != nil {
+		writeError(w, err)
+	}
+
+	defer conn.Close()
+
+	qry, err := conn.QueryContext(context.Background(), "SELECT * FROM users")
+
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+
+	result := make([]User, 0)
+
+	for qry.Next() {
+		var id sql.NullInt32
+		var firstName sql.NullString
+		var lastName sql.NullString
+		var birth sql.NullTime
+
+		err = qry.Scan(&id, &firstName, &lastName, &birth)
+		if err != nil {
+			writeError(w, err)
+			return
+		}
+
+		user := User{}
+		user.ID = int(id.Int32)
+		user.FirstName = firstName.String
+		user.LastName = lastName.String
+		user.Birth = birth.Time
+
+		result = append(result, user)
+	}
+
+	writeData(w, result)
+}
+
+func createUserHandler(w http.ResponseWriter, r *http.Request) {
+	payload := new(User)
+
+	err := json.NewDecoder(r.Body).Decode(payload)
+
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+
+	conn, err := conn()
+
+	if err != nil {
+		writeError(w, err)
+	}
+
+	defer conn.Close()
+
+	stmt, err := conn.PrepareContext(context.Background(), "INSERT INTO users (first_name, last_name, birth) VALUES(?,?,?)")
+
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+
+	stmtRes, err := stmt.ExecContext(context.Background(), payload.FirstName, payload.LastName, payload.Birth)
+
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+
+	id, _ := stmtRes.LastInsertId()
+
+	result := map[string]interface{}{"LastInsertID" : id}
+
+	writeData(w,result)
 }
